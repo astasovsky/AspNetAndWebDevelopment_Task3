a406ce545623d436fc4b8c8007cdcb4972cbe938
astasovsky <stasovsky.ua@gmail.com>
2022-11-08 22:24:32 +0200

    Remade tic-tac-toe game from Task 2 using ASP.NET Forms

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using AspNetAndWebDevelopment_Task3.Models;
+﻿using System.Diagnostics;
+using AspNetAndWebDevelopment_Task3.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace AspNetAndWebDevelopment_Task3.Controllers
 {
@@ -18,9 +18,194 @@ namespace AspNetAndWebDevelopment_Task3.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Route("Home/GetInfo/{a1?}/{a2?}/{a3?}/{b1?}/{b2?}/{b3?}/{c1?}/{c2?}/{c3?}")]
+        public IActionResult GetInfo(string a1, string a2, string a3, string b1, string b2, string b3, string c1,
+            string c2, string c3)
         {
-            return View();
+            List<string> list = new() { a1, a2, a3, b1, b2, b3, c1, c2, c3 };
+            string[] win1 = { a1, a2, a3 };
+            string[] win2 = { b1, b2, b3 };
+            string[] win3 = { c1, c2, c3 };
+
+            string[] win4 = { a1, b1, c1 };
+            string[] win5 = { a2, b2, c2 };
+            string[] win6 = { a3, b3, c3 };
+
+            string[] win7 = { a1, b2, c3 };
+            string[] win8 = { a3, b2, c1 };
+
+            string[][] winComb = { win1, win2, win3, win4, win5, win6, win7, win8 };
+
+            foreach (string[] e in winComb)
+            {
+                int compare1 = string.CompareOrdinal(e[0], e[1]);
+                int compare2 = string.CompareOrdinal(e[1], e[2]);
+                int compare3 = string.CompareOrdinal(e[0], e[2]);
+                if (compare1 == 0
+                    && compare2 == 0
+                    && compare3 == 0
+                    && !string.IsNullOrEmpty(e[0])
+                    && !string.IsNullOrEmpty(e[1])
+                    && !string.IsNullOrEmpty(e[2]))
+                {
+                    list.Add($"won{e[0]}");
+                    return Json(list);
+                }
+
+                int status = 0;
+                foreach (string l in list)
+                {
+                    if (l == null) status++;
+                }
+
+                if (status == 0)
+                {
+                    list.Add("draw");
+                    return Json(list);
+                }
+            }
+
+
+            if (a1 == "O" && a2 == "O" && a3 == null) list[2] = "O";
+            else if (a1 == "O" && a2 == null && a3 == "O") list[1] = "O";
+            else if (a1 == null && a2 == "O" && a3 == "O") list[0] = "O";
+
+            else if (b1 == "O" && b2 == "O" && b3 == null) list[5] = "O";
+            else if (b1 == "O" && b2 == null && b3 == "O") list[4] = "O";
+            else if (b1 == null && b2 == "O" && b3 == "O") list[3] = "O";
+
+            else if (c1 == "O" && c2 == "O" && c3 == null) list[8] = "O";
+            else if (c1 == "O" && c2 == null && c3 == "O") list[7] = "O";
+            else if (c1 == null && c2 == "O" && c3 == "O") list[6] = "O";
+
+            else if (a1 == "O" && b1 == "O" && c1 == null) list[6] = "O";
+            else if (a1 == "O" && b1 == null && c1 == "O") list[3] = "O";
+            else if (a1 == null && b1 == "O" && c1 == "O") list[0] = "O";
+
+            else if (a2 == "O" && b2 == "O" && c2 == null) list[7] = "O";
+            else if (a2 == "O" && b2 == null && c2 == "O") list[4] = "O";
+            else if (a2 == null && b2 == "O" && c2 == "O") list[1] = "O";
+
+            else if (a3 == "O" && b3 == "O" && c3 == null) list[8] = "O";
+            else if (a3 == "O" && b3 == null && c3 == "O") list[5] = "O";
+            else if (a3 == null && b3 == "O" && c3 == "O") list[2] = "O";
+
+            else if (a1 == "O" && b2 == "O" && c3 == null) list[8] = "O";
+            else if (a1 == "O" && b2 == null && c3 == "O") list[4] = "O";
+            else if (a1 == null && b2 == "O" && c3 == "O") list[0] = "O";
+
+            else if (a3 == "O" && b2 == "O" && c1 == null) list[6] = "O";
+            else if (a3 == "O" && b2 == null && c1 == "O") list[4] = "O";
+            else if (a3 == null && b2 == "O" && c1 == "O") list[2] = "O";
+
+
+            else if (a1 == "X" && a2 == "X" && a3 == null) list[2] = "O";
+            else if (a1 == "X" && a2 == null && a3 == "X") list[1] = "O";
+            else if (a1 == null && a2 == "X" && a3 == "X") list[0] = "O";
+
+            else if (b1 == "X" && b2 == "X" && b3 == null) list[5] = "O";
+            else if (b1 == "X" && b2 == null && b3 == "X") list[4] = "O";
+            else if (b1 == null && b2 == "X" && b3 == "X") list[3] = "O";
+
+            else if (c1 == "X" && c2 == "X" && c3 == null) list[8] = "O";
+            else if (c1 == "X" && c2 == null && c3 == "X") list[7] = "O";
+            else if (c1 == null && c2 == "X" && c3 == "X") list[6] = "O";
+
+            else if (a1 == "X" && b1 == "X" && c1 == null) list[6] = "O";
+            else if (a1 == "X" && b1 == null && c1 == "X") list[3] = "O";
+            else if (a1 == null && b1 == "X" && c1 == "X") list[0] = "O";
+
+            else if (a2 == "X" && b2 == "X" && c2 == null) list[7] = "O";
+            else if (a2 == "X" && b2 == null && c2 == "X") list[4] = "O";
+            else if (a2 == null && b2 == "X" && c2 == "X") list[1] = "O";
+
+            else if (a3 == "X" && b3 == "X" && c3 == null) list[8] = "O";
+            else if (a3 == "X" && b3 == null && c3 == "X") list[5] = "O";
+            else if (a3 == null && b3 == "X" && c3 == "X") list[2] = "O";
+
+            else if (a1 == "X" && b2 == "X" && c3 == null) list[8] = "O";
+            else if (a1 == "X" && b2 == null && c3 == "X") list[4] = "O";
+            else if (a1 == null && b2 == "X" && c3 == "X") list[0] = "O";
+
+            else if (a3 == "X" && b2 == "X" && c1 == null) list[6] = "O";
+            else if (a3 == "X" && b2 == null && c1 == "X") list[4] = "O";
+            else if (a3 == null && b2 == "X" && c1 == "X") list[2] = "O";
+
+
+            else
+            {
+                Random random = new();
+                while (true)
+                {
+                    int rand = random.Next(0, 8);
+
+                    if (list[rand] == null)
+                    {
+                        list[rand] = "O";
+                        break;
+                    }
+                }
+            }
+
+            win1[0] = list[0];
+            win1[1] = list[1];
+            win1[2] = list[2]; // 1
+            win2[0] = list[3];
+            win2[1] = list[4];
+            win2[2] = list[5]; // 2
+            win3[0] = list[6];
+            win3[1] = list[7];
+            win3[2] = list[8]; // 3
+
+            win4[0] = list[0];
+            win4[1] = list[3];
+            win4[2] = list[6]; // 4
+            win5[0] = list[1];
+            win5[1] = list[4];
+            win5[2] = list[7]; // 5
+            win6[0] = list[2];
+            win6[1] = list[5];
+            win6[2] = list[8]; // 6
+
+            win7[0] = list[0];
+            win7[1] = list[4];
+            win7[2] = list[8]; // 7
+            win8[0] = list[2];
+            win8[1] = list[4];
+            win8[2] = list[6]; // 8
+
+            string[][] result = { win1, win2, win3, win4, win5, win6, win7, win8 };
+
+            foreach (string[] e in result)
+            {
+                int compare1 = string.CompareOrdinal(e[0], e[1]);
+                int compare2 = string.CompareOrdinal(e[1], e[2]);
+                int compare3 = string.CompareOrdinal(e[0], e[2]);
+                if (compare1 == 0
+                    && compare2 == 0
+                    && compare3 == 0
+                    && !string.IsNullOrEmpty(e[0])
+                    && !string.IsNullOrEmpty(e[1])
+                    && !string.IsNullOrEmpty(e[2]))
+                {
+                    list.Add($"won{e[0]}");
+                    return Json(list);
+                }
+
+                int status = 0;
+                foreach (string l in list)
+                {
+                    if (l == null) status++;
+                }
+
+                if (status == 0)
+                {
+                    list.Add("draw");
+                    return Json(list);
+                }
+            }
+
+            return Json(list);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,90 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    var x = 'X';
+    var o = 'O';
+    var empty = '';
+
+    $(document).click(function (e) {
+        let elem = $(e.target);
+        if (elem.html() === empty && elem.hasClass('field')) {
+            elem.html(x);
+            setTimeout(doStep, 300);
+        } else if (elem.html() === empty && elem.hasClass('field')) {
+            elem.html(o);
+            setTimeout(doStep, 300);
+        } else if (elem.html() !== empty && elem.hasClass('field')) {
+            alert('Ячейка занята!');
+        }
+    });
+
+    $(".btn-game").click(function () {
+        location.reload();
+    })
+
+    function doStep() {
+        a1 = $('#a1').html();
+        a2 = $('#a2').html();
+        a3 = $('#a3').html();
+
+        b1 = $('#b1').html();
+        b2 = $('#b2').html();
+        b3 = $('#b3').html();
+
+        c1 = $('#c1').html();
+        c2 = $('#c2').html();
+        c3 = $('#c3').html();
+
+
+        $.get('Home/GetInfo/', {
+            a1: a1,
+            a2: a2,
+            a3: a3,
+            b1: b1,
+            b2: b2,
+            b3: b3,
+            c1: c1,
+            c2: c2,
+            c3: c3
+        }, function (data) {
+            $('.text-danger').html(data);
+            console.log(data);
+
+            $("#a1").html(data[0]);
+            $("#a2").html(data[1]);
+            $("#a3").html(data[2]);
+            $("#b1").html(data[3]);
+            $("#b2").html(data[4]);
+            $("#b3").html(data[5]);
+            $("#c1").html(data[6]);
+            $("#c2").html(data[7]);
+            $("#c3").html(data[8]);
+
+            if (data[9] !== undefined) {
+                let wonText;
+                if (data[9] === "wonX") {
+                    wonText = "Вы победили!!!";
+                } else if (data[9] === "wonO") {
+                    wonText = "Вы проиграли!";
+                } else {
+                    wonText = "Ничья!";
+                }
+                setTimeout(function () {
+                    $(".winner").html("<h2>" + wonText + "</h2> ")
+                }, 400);
+                $('#a1').removeClass("field");
+                $('#a2').removeClass("field");
+                $('#a3').removeClass("field");
+
+                $('#b1').removeClass("field");
+                $('#b2').removeClass("field");
+                $('#b3').removeClass("field");
+
+                $('#c1').removeClass("field");
+                $('#c2').removeClass("field");
+                $('#c3').removeClass("field");
+            }
+        });
+    }
+});
